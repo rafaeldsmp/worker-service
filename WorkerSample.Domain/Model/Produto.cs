f6986aa1c66d2d9925381228606fd1380657914c
Rafael Pino <rafael.pino@capgemini.com>
2023-07-07 14:16:27 -0300

    feature: adicionado domain com a model produto

---
+++ WorkerSample.Domain/Model/Produto.cs
@@ -0,0 +1,12 @@
+﻿namespace WorkerSample.Domain.Model
+{
+    public class Produto
+    {
+        public string? Categoria { get; set; }
+        public string? Tipo { get; set; }
+        public string? Nome { get; set; }
+        public int Quantidade { get; set; }
+        public double Preco { get; set; }
+        public string? Estado { get; set; }
+    }
+}
