09ad431da513b4062132b5982495d6bf8f113736
Rafael Pino <rafael.pino@capgemini.com>
2023-07-10 17:20:18 -0300

    feat: implementado NLog na classe program

--- WorkerSample/Program.cs
+++ WorkerSample/Program.cs
@@ -1,15 +1,19 @@
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
+using NLog;
+using NLog.Extensions.Logging;
 using WorkerSample;
 using WorkerSample.Domain.IRepository;
 using WorkerSample.Infrastructure.Data;
 using WorkerSample.Infrastructure.Repository;
+using ILogger = NLog.ILogger;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
         services.AddHostedService<Worker>();
     })
+
     .UseServiceProviderFactory(new AutofacServiceProviderFactory())
     .ConfigureContainer<ContainerBuilder>((builder) =>
     {
@@ -20,8 +24,17 @@ IHost host = Host.CreateDefaultBuilder(args)
             return option;
         }).InstancePerLifetimeScope();
         builder.RegisterType<ProductRepository>().As<IProductRepository>().InstancePerLifetimeScope();
+        builder.RegisterInstance(LogManager.GetCurrentClassLogger()).As<ILogger>().As<Logger>().SingleInstance();
     })
     
+    .ConfigureLogging((ctx, logging)=>
+    {
+        logging.ClearProviders();
+        logging.AddNLog(ctx.Configuration, new NLogProviderOptions()
+        {
+            LoggingConfigurationSectionName = "NLog"
+        });
+    })
     .Build();
 
 await host.RunAsync();
